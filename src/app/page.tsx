c43bf31f614d1a83bfb74f87e8cda7f27310354f
JustineTaniardi <taniardijustine@gmail.com>
2025-11-09 15:42:12 +0700

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,6 @@ export default function HomePage() {
   return (
     <main className="flex flex-col items-center bg-white text-black">
       {/* Bagian utama dengan id */}
-      <section id="home" className="min-h-screen flex flex-col justify-center items-center text-center bg white text-black ">
-        <h1 className="text-5xl font-extrabold mb-4">
-          Lihat Waktumu, Rasakan Perubahan
-        </h1>
-        <p className="max-w-xl text-gray-600">
-          Kinote membantumu mengatur tugas, melacak aktivitas, dan memahami progres diri dalam satu ruang yang jernih.
-        </p>
-        <button className="mt-6 bg-gray-900 text-white px-6 py-3 rounded-md hover:bg-gray-800 transition">
-          Lihat Fitur
-        </button>
-      </section>
 
       {/* Section Feature */}
       <section id="feature" className="min-h-screen flex flex-col items-center justify-center bg-gray-50 w-full">
